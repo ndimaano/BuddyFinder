00412e1fd5a8941b260c8fd5e67f0817ac482d00
Xichen Gao <xichenga@usc.edu>
2023-11-28 00:37:55 -0800

    Initial Commit

---
+++ BuddyFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BuddyFinder
+//
+//  Created by Xichen Gao on 11/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
