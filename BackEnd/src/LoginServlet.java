7a94249bed823ec35538167e21a207dc523fdc3c
Kasia Felkel <kfelkel@usc.edu>
2023-11-29 20:49:31 -0800

    Update LoginServlet.java

--- BackEnd/src/LoginServlet.java
+++ BackEnd/src/LoginServlet.java
@@ -1,60 +1,52 @@
-package Servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 @WebServlet("/Login")
 public class LoginServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
+	
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/your-database";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASSWORD = "password";
 
-    public LoginServlet() {
-        super(); 
-    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        String username = request.getParameter("login-username");
+        String password = request.getParameter("login-password");
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
-        throws ServletException, IOException { 
-        PrintWriter out = response.getWriter(); 
-        String username = request.getParameter("login-username"); 
-        String password = request.getParameter("login-password"); 
-        System.out.println(username + " " + password);
-
-        try (Connection conn = JDBCConnector.getConnection();
+        try (Connection conn = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
              PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users WHERE username = ? AND password = ?")) {
-
             stmt.setString(1, username);
             stmt.setString(2, password);
             ResultSet rs = stmt.executeQuery();
+
+            JsonObject jsonResponse = new JsonObject();
             if (rs.next()) {
-                HttpSession session = request.getSession();
-                Cookie sessionCookie = StudentTracker.setUserSession(username, session);
-                response.addCookie(sessionCookie);
-                response.sendRedirect("index.html"); // Ensure this is the correct redirect
-                out.println("Successfully logged in");
+                jsonResponse.addProperty("status", "success");
             } else {
-                response.setContentType("text/html");
-                out.println("<script type=\"text/javascript\">");
-                out.println("alert('Invalid credentials');");
-                out.println("</script>");
-                System.out.println("Invalid credentials: " + username + " " + password);
+                jsonResponse.addProperty("status", "failure");
             }
+            out.println(new Gson().toJson(jsonResponse));
         } catch (SQLException e) {
             e.printStackTrace();
-            // Consider more comprehensive error handling
-        } finally {
-            out.flush();
-            out.close();
+            JsonObject jsonResponse = new JsonObject();
+            jsonResponse.addProperty("status", "error");
+            out.println(new Gson().toJson(jsonResponse));
         }
     }
 }
