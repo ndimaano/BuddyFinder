0c0537a729f761bc0bd353aaca4c39834ee19dc8
Kasia Felkel <kfelkel@usc.edu>
2023-11-29 20:51:29 -0800

    Update SignUpServlet.java

--- BackEnd/src/SignUpServlet.java
+++ BackEnd/src/SignUpServlet.java
@@ -1,80 +1,47 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 @WebServlet("/SignUp")
 public class SignUpServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
-    public SignUpServlet() {
-        super();
-    }
+	private static final long serialVersionUID = 1L;
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/your-database";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASSWORD = "password";
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        response.setContentType("application/json");
         PrintWriter out = response.getWriter();
         String username = request.getParameter("signup-username");
         String email = request.getParameter("signup-email");
         String password = request.getParameter("signup-password");
 
-        if (!ifStudentExists(email, username)) {
-            try {
-                JDBCConnector.insertUser(email, username, password);
-                HttpSession session = request.getSession();
-                Cookie sessionCookie = StudentTracker.setUserSession(username, session);
-                response.addCookie(sessionCookie);
-                response.sendRedirect("index.html"); // Redirect to a meaningful page
-            } catch (Exception e) {
-                response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-                out.println("Error during sign up: " + e.getMessage());
-            }
-        } else {
-            response.setContentType("text/plain");
-            response.setCharacterEncoding("UTF-8");
-            out.println("User already exists");
-        }
-
-        out.flush();
-        out.close();
-    }
-
-    private boolean ifStudentExists(String email, String username) {
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        try {
-            conn = JDBCConnector.getConnection();
-            //  check duplicate email
-            stmt = conn.prepareStatement("SELECT email FROM users WHERE email = ?");
-            stmt.setString(1, email);
-            if (stmt.executeQuery().next()) {
-                return true;
-            }
-            stmt.close();
-            // check duplicate username
-            stmt = conn.prepareStatement("SELECT username FROM users WHERE username = ?");
+        try (Connection conn = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+             PreparedStatement stmt = conn.prepareStatement("INSERT INTO users (username, email, password) VALUES (?, ?, ?)")) {
             stmt.setString(1, username);
-            return stmt.executeQuery().next();
+            stmt.setString(2, email);
+            stmt.setString(3, password);
+
+            int rowsAffected = stmt.executeUpdate();
+            JsonObject jsonResponse = new JsonObject();
+            jsonResponse.addProperty("status", rowsAffected > 0 ? "success" : "failure");
+            out.println(new Gson().toJson(jsonResponse));
         } catch (SQLException e) {
             e.printStackTrace();
-            return true;
-        } finally {
-            try {
-                if (stmt != null) stmt.close();
-                if (conn != null) conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+            JsonObject jsonResponse = new JsonObject();
+            jsonResponse.addProperty("status", "error");
+            out.println(new Gson().toJson(jsonResponse));
         }
-        return false;
     }
 }
