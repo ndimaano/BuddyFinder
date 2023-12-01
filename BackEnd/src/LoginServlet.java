c83d80b40f7e37ba378eb644ce5ecde4d9067a2c
Kasia Felkel <kfelkel@usc.edu>
2023-11-30 21:10:28 -0800

    Update LoginServlet.java

--- BackEnd/src/LoginServlet.java
+++ BackEnd/src/LoginServlet.java
@@ -18,17 +18,15 @@ import com.google.gson.JsonObject;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/your-database";
-    private static final String JDBC_USER = "root";
-    private static final String JDBC_PASSWORD = "password";
-
+    private static final String JDBC_URL = "jdbc:mysql://localhost/buddyFinder?user=root&password=root";
+	
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("application/json");
         PrintWriter out = response.getWriter();
         String username = request.getParameter("login-username");
         String password = request.getParameter("login-password");
-
+	Class.forName("com.mysql.cj.jdbc.Driver");
         try (Connection conn = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
              PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users WHERE username = ? AND password = ?")) {
             stmt.setString(1, username);
